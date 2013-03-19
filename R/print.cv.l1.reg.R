50885a81292302aa6df808b874a8d93116bae211
Edward Grant <edward.m.grant@gmail.com>
2013-03-19 00:00:00 +0000

    version 1.0

---
+++ R/print.cv.l1.reg.R
@@ -0,0 +1,9 @@
+print.cv.l1.reg <-
+function (x, ...)
+{
+  cat ("\n Call: \n")
+  print (x$call)
+  
+  cat ("\n Selected Coefficient Estimates: \n")
+  print (cbind(Lambda=x$lam.vec,"# Selected"=x$num.pred, "CV Error"=x$mean.error),justify="centre")
+}
---
+++ R/print.cv.l2.reg.R
@@ -0,0 +1,10 @@
+print.cv.l2.reg <-
+function (x, ...)
+{
+  cat ("\n Call: \n")
+  print (x$call)
+  
+  cat ("\n Selected Coefficient Estimates: \n")
+  print (cbind(Lambda=x$lam.vec,"# Selected"=x$num.pred, "CV Error"=x$mean.error),justify="centre")
+
+}
